3c9f70bbe622c73da9104ec288922fb07ce6a766
youssefsaad121000-crypto <youssefsaad121000@gmail.com>
2026-07-30 17:54:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,28 @@
 const mineflayer = require('mineflayer');
-const util = require('util');
 
-const HOST = 'thshesh.aternos.me';
-const PORT = 17442;
-const USERNAME = 'BOT_';
-const VERSION = '1.21.1';
+const bot = mineflayer.createBot({
+  host: 'thshesh.aternos.me',
+  port: 17442,
+  username: 'GoodMiner',
+  version: '1.21.1'
+});
 
-let reconnecting = false;
+bot.once('login', () => {
+  console.log('Logged in.');
+});
 
-function createBot() {
-  const bot = mineflayer.createBot({
-    host: HOST,
-    port: PORT,
-    username: USERNAME,
-    version: VERSION
-  });
+bot.once('spawn', () => {
+  console.log('Spawned.');
+});
 
-  console.log('Connecting...');
+bot.on('kicked', (reason) => {
+  console.log('Kicked:', reason);
+});
 
-  bot.once('spawn', () => {
-    reconnecting = false;
-    console.log('Spawned.');
-  });
+bot.on('error', (err) => {
+  console.log('Error:', err);
+});
 
-  function reconnect() {
-    if (reconnecting) return;
-    reconnecting = true;
-
-    console.log('Reconnecting in 5 seconds...');
-
-    setTimeout(() => {
-      createBot();
-    }, 5000);
-  }
-
-  bot.on('kicked', (reason) => {
-    console.log('Kicked:');
-    console.log(util.inspect(reason, { depth: null, colors: true }));
-    reconnect();
-  });
-
-  bot.on('end', () => {
-    console.log('Disconnected.');
-    reconnect();
-  });
-
-  bot.on('error', (err) => {
-    console.log('Error:');
-    console.log(err);
-  });
-}
-
-createBot();
+bot.on('end', () => {
+  console.log('Disconnected.');
+});
